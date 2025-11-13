532da8c39da2dc74dccb9fa71230d2559eb2950f
Pennetz <168197649+Pennetz@users.noreply.github.com>
2025-11-13 21:46:56 +0000

    Aggiunta cartella per il progetto Jungleon. Dentro la cartella è stata fatta una paginetta base (index) che porta ai 3 diagrammi (casi d'uso, Er, classi)

---
+++ Progetti/Jungleon/index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="it">
+    <head>
+        <title>Fabrizio Pennetta Jungleon</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+            <div class="container">
+                <div class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php">Back to Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="../Informatica.php">Informatica</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../TPSIT.php">TPSIT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../GPO.php">GPO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Jungleon</a>
+                    </li>
+                </div>
+            </div>
+            
+        </header>
+        <main>
+
+
+            <ul class="list-group">
+                <li class="list-group-item"><a href="./diagrammaER.html">Diagramma ER</a></li>
+                <li class="list-group-item"><a href="./diagrammaClassi.html">Diagramma delle classi</a></li>
+                <li class="list-group-item"><a href="./diagrammaCasiDuso.html">Diagramma casi d'uso</a></li>
+            </ul>
+            
+
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
