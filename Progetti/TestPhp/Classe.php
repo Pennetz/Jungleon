f9c5dc3a4ca6a117eb3a0e59c85dcb98a1de3328
Pennetz <168197649+Pennetz@users.noreply.github.com>
2026-02-25 17:16:36 +0000

    modifiche

---
+++ Progetti/TestPhp/Classe.php
@@ -0,0 +1,8 @@
+<?php
+final class Greeter
+{
+    public function greet(string $name): string
+    {
+        return 'Hello, ' . $name . '!';
+    }
+}
---
+++ Progetti/TestPhp/Test.php
@@ -0,0 +1,15 @@
+<?php
+require_once "Classe.php";
+use PHPUnit\Framework\TestCase;
+
+final class GreeterTest extends TestCase
+{
+    public function testGreetsWithName(): void
+    {
+        $greeter = new Greeter;
+
+        $greeting = $greeter->greet('Alice');
+
+        $this->assertSame('Hello, Alice!', $greeting);
+    }
+}
